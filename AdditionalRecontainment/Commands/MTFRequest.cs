1850db6a3189a783b72025a399f06a4ebab12904
Oleg <super.vec2013@gmail.com>
2021-08-11 20:02:09 +0300

    Добавьте файлы проекта.

---
+++ AdditionalRecontainment/Commands/MTFRequest.cs
@@ -0,0 +1,36 @@
+﻿using CommandSystem;
+using RemoteAdmin;
+using System;
+using Exiled.API;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+namespace AdditionalRecontainment.Commands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    class MTFRequest : ICommand
+    {
+        public string Command { get; } = "Request";
+
+        public string[] Aliases { get; } = { };
+
+        public string Description { get; } = "request mtf helicopter";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out RespawnEffectType pls)
+        {
+            if (sender is PlayerCommandSender)
+            {
+                RespawnEffectType.SummonNtfChopper
+                return true;
+            }
+            else
+            {
+                pls = RespawnEffectType.
+                return false;
+            }
+        }
+    }
+}
