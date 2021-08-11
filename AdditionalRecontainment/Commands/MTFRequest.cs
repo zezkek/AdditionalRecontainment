de9804941e8cac69441dd24d6f9f9b21187a00bb
Oleg <super.vec2013@gmail.com>
2021-08-12 02:35:34 +0300

    Command will work only for ntf players on helipad

--- AdditionalRecontainment/Commands/MTFRequest.cs
+++ AdditionalRecontainment/Commands/MTFRequest.cs
@@ -8,27 +8,48 @@ using System.Text;
 using System.Threading.Tasks;
 using Exiled.API.Enums;
 using Exiled.API.Features;
+using UnityEngine;
+
 namespace AdditionalRecontainment.Commands
 {
-    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    [CommandHandler(typeof(ClientCommandHandler))]
     class MTFRequest : ICommand
     {
-        public string Command { get; } = "Request";
+        public string Command { get; } = "request";
 
         public string[] Aliases { get; } = { };
 
         public string Description { get; } = "request mtf helicopter";
-
-        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out RespawnEffectType pls)
+        Vector3 mtfposition = new Vector3(163, 990, -52);
+        Vector3 mtfposition2 = new Vector3(193,995,-68);
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
-            if (sender is PlayerCommandSender)
+            Player player_requester = Player.Get((sender as CommandSender)?.SenderId);
+            if (player_requester.IsHuman)
             {
-                RespawnEffectType.SummonNtfChopper
-                return true;
+                if (player_requester.IsNTF)
+                {
+                    if (player_requester.Position.x > mtfposition.x && player_requester.Position.x < mtfposition2.x
+                        && player_requester.Position.y > mtfposition.y && player_requester.Position.y < mtfposition2.y
+                        && player_requester.Position.z < mtfposition.z && player_requester.Position.z > mtfposition2.z)
+                    {
+                        
+                        Respawn.PlayEffect(RespawnEffectType.SummonNtfChopper);
+                        response = "Success";
+                        return true;
+                    }
+                }
+                if (player_requester.Team == Team.CHI)
+                {
+                    response = "Success";
+                    return true;
+                }
+                response = "failed";
+                return false;
             }
             else
             {
-                pls = RespawnEffectType.
+                response = "failed";
                 return false;
             }
         }
--- AdditionalRecontainment/Plugin.cs
+++ AdditionalRecontainment/Plugin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Exiled.API.Features;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace AdditionalRecontainment
 {
-    public class Plugin
+    public class Plugin : Plugin<Config>
     {
+        public override string Name { get; } = "AdditionalRecontainment";
+        public override string Author { get; } = ".fkn_goose";
+        public override Version Version => new Version(0, 1, 0);
+        public static readonly Lazy<Plugin> LazyInstance = new Lazy<Plugin>(valueFactory: () => new Plugin());
+        public static Plugin PluginItem => LazyInstance.Value;
     }
 }
